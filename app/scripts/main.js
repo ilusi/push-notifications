d03395ede71bb58d82ba1f2ef49a4d5b6e378ca8
Chip Salim <ilusi@yahoo.com>
2018-01-30 11:54:09 -0500

    initial

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,8 +21,7 @@
 
 'use strict';
 
-const applicationServerPublicKey = '<Your Public Key>';
-
+const applicationServerPublicKey = 'BNSBinXn9dJrHSQhyV9zIni5QPxNfrJEdqof_PoJO5KoDPGpZQrPcbP0PHpAlsiIT9QsjpXYqTnYG9UNW5w4pqY';
 const pushButton = document.querySelector('.js-push-btn');
 
 let isSubscribed = false;
@@ -42,3 +41,141 @@ function urlB64ToUint8Array(base64String) {
   }
   return outputArray;
 }
+
+// Register our Service Worker.d
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  navigator.serviceWorker.register('sw.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+
+    swRegistration = swReg;
+
+    // call initializeUI() when our service worker is registered.
+    initializeUI();
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
+}
+
+
+// check if the user is currently subscribed
+function initializeUI() {
+  // Prevent double submission.
+  pushButton.addEventListener('click', function() {
+    pushButton.disabled = true;
+
+    if (isSubscribed) {
+      // TODO: Unsubscribe user
+    } else {
+      subscribeUser();
+    }
+  });
+
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    updateSubscriptionOnServer(subscription);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      console.log('User is NOT subscribed.');
+    }
+
+    updateBtn();
+  });
+}
+
+// enable our button and change the text if the user is subscribed or not.
+function updateBtn() {
+  // if the user blocks the permission request. This needs some unique consideration because if the user blocks the permission, our web app will not be able to re-show the permission prompt and will not be able to subscribe the user, so we need to at least disable the push button so the user knows it can't be used.
+  if (Notification.permission === 'denied') {
+    pushButton.textContent = 'Push Messaging Blocked.';
+    pushButton.disabled = true;
+    updateSubscriptionOnServer(null);
+    return;
+  }
+
+  if (isSubscribed) {
+    pushButton.textContent = 'Disable Push Messaging';
+  } else {
+    pushButton.textContent = 'Enable Push Messaging';
+  }
+
+  pushButton.disabled = false;
+}
+
+/**
+ Calling subscribe() returns a promise which will resolve after the following steps:
+
+ The user has granted permission to display notifications.
+ The browser has sent a network request to a push service to get the details to generate a PushSubscription.
+ */
+function subscribeUser() {
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true, // show a notification every time a push is sent
+    applicationServerKey: applicationServerKey
+  })
+  .then(function(subscription) {
+    console.log('User is subscribed.');
+
+    updateSubscriptionOnServer(subscription);
+
+    isSubscribed = true;
+
+    updateBtn();
+  })
+  .catch(function(err) {
+    console.log('Failed to subscribe the user: ', err);
+    updateBtn();
+  });
+}
+
+function updateSubscriptionOnServer(subscription) {
+  // TODO: Send subscription to application server
+
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails =
+    document.querySelector('.js-subscription-details');
+
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    subscriptionDetails.classList.remove('is-invisible');
+  } else {
+    subscriptionDetails.classList.add('is-invisible');
+  }
+}
+
+/*
+ When we trigger a push message, the browser receives the push message,
+ figures out what service worker the push is for before waking up that 
+ service worker and dispatching a push event. We need to listen for this 
+ event and show a notification as a result.
+ self === service worker
+ */
+ self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: 'Yay it works.',
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+  // waitUntil method takes a promise and the browser will keep your service worker alive and running until the promise passed in has resolved.
+  //event.waitUntil(self.registration.showNotification(title, options));
+  // or re-write like this for better readibility.
+  const notificationPromise = self.registration.showNotification(title, options);
+  event.waitUntil(notificationPromise);
+});
